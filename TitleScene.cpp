3461698decba32e97b06b74a8f430237390735e4
Mitsuki_1601323 <1601323@st.asojuku.ac.jp>
2018-07-30 15:15:34 +0900

    tuika

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,7 +1,7 @@
 #include "TitleScene.h"
-#include "DiffSelectScene.h"
 //音をならすためにinclude
 #include "SimpleAudioEngine.h"
+#include "CharaSelectScene.h"
 
 Scene *TitleScene::createScene()
 {
@@ -26,18 +26,16 @@ bool TitleScene::init()
 	Size winSize = Director::getInstance()->getWinSize();
 
 	// ボタン配置
-	// 通常時,押した時
-	// 押した時のｱｸｼｮﾝ
-	auto startButton = MenuItemImage::create("CloseNormal.png","CloseSelected.png", CC_CALLBACK_1(TitleScene::pushStart,this));
+	auto startButton = MenuItemImage::create("CloseNormal.png", "CloseSelected.png", CC_CALLBACK_1(TitleScene::pushStart, this));
 
 	// ボタンの配置
-	startButton->setPosition(Point(winSize.width/2,winSize.height/2));
+	startButton->setPosition(Point(winSize.width / 2, winSize.height / 2));
 	// メニュー作成(自動obj)
-	auto menu = Menu::create(startButton,NULL);
+	auto menu = Menu::create(startButton, NULL);
 	menu->setPosition(Point::ZERO);
 	// 追加
-	this->addChild(menu,1);
-	
+	this->addChild(menu, 1);
+
 	return true;
 }
 
@@ -49,25 +47,11 @@ void TitleScene::TitleBackGroudn()
 	// マルチレゾリューション対応がどうとか
 	Point origin = Director::getInstance()->getVisibleOrigin();
 
-	//// バックカラー
-	//auto groundCollar = LayerColor::create(Color4B::BLUE,winSize.width,winSize.height);
-	//// バックグランドCollar第2:表示順
-	//this->addChild(groundCollar,0);
-
-	// タイトル配置
-	// 配置文字
-	auto lbl_Title = Label::createWithSystemFont("Title","HiraKakuProN-W6",100);
-	// 配置場所
-	lbl_Title->setPosition(Point(origin.x+winSize.width/2,
-								 origin.y+winSize.height - lbl_Title->getContentSize().height));
-	// Title追加
-	this->addChild(lbl_Title,1);
-
 	// 背景画像追加
-	Sprite* bgsprite = Sprite::create("Title.png");
+	Sprite* bgsprite = Sprite::create("BackImage/Title.png");
 	// 表示座標指定
-	bgsprite->setPosition(ccp(320, 190));
-	this->addChild(bgsprite,0);
+	bgsprite->setPosition(winSize.width/2,winSize.height/2);
+	this->addChild(bgsprite, 0);
 }
 
 // 画面遷移
@@ -77,17 +61,9 @@ void TitleScene::pushStart(Ref *pSender)
 	CCLOG("Pushボタン");
 
 	// 遷移策の画面をｲﾝｽﾀﾝｽ
-	Scene *pScene = DiffSelectScene::createScene();
-
-	/* 0.5秒かけて次画面に遷移
-	// (時間,遷移先,色(オプション))
-	//TransitionFade *transition = TransitionFade::create(0.6, pScene);
-	// 遷移実行 アニメーション
-	Director::getInstance()->replaceScene(transition);*/
+	Scene *pScene = CharaSelectScene::createScene();
 
 	// 模様左からだんだん...
-	Director::getInstance()->replaceScene(TransitionFadeTR::create(0.7f, DiffSelectScene::createScene()));
-	CCDirector::sharedDirector()->replaceScene(CCTransitionFadeTR::create(2.0f, DiffSelectScene::createScene()));
-
-
+	Director::getInstance()->replaceScene(TransitionFadeTR::create(0.7f, CharaSelectScene::createScene()));
+	CCDirector::sharedDirector()->replaceScene(CCTransitionFadeTR::create(2.0f, CharaSelectScene::createScene()));
 }
